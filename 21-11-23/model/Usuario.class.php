2c318a7adab91d56b1cb92e25dd79ae35c6dd976
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2023-11-21 16:53:37 -0300

    Update Usuario.class.php

--- 21-11-23/model/Usuario.class.php
+++ 21-11-23/model/Usuario.class.php
@@ -38,6 +38,36 @@
      }
      echo '</table>';
     }
+
+    public function relatorioNotas (){
+      $pdo = new pdo("mysql:host=localhost; dbname=escola_201123","root","");
+      $consulta = "select nome, nota from aluno inner join notas on aluno.id = notas.id_aluno";
+
+     $consultar_aluno_notas = $pdo->prepare($consulta);
+     $consultar_aluno_notas->execute();
+     
+     
+     $consulta = "select nome_disc, nome from professor inner join disciplina on professor.id = disciplina.id_professor";
+     $consultar_disc_prof = $pdo->prepare($consulta);
+     $consultar_disc_prof->execute();
+
+     
+     echo '<table border=\'1\'>';
+     echo '<tr><td>Nome</td><td>Nota</td><td>Disciplina</td><td>Profesor</td>';
+     
+     foreach ($consultar_aluno_notas as $value){
+       echo '<tr><td>' . $value['nome'] . '</td>'; 
+       echo '<td>' . $value['nota'] . '</td>';
+
+      }
+      echo $consultar_disc_prof['nome_disc'];
+     foreach ($consultar_disc_prof as $value){
+        echo '<td>' . $value['nome_disc'] . '</td></tr>'; 
+        echo '<td>' . $value['nome'] . '</td>'; 
+     }
+     echo '</table>';
+    }
+
     public function autenticar($email, $senha){
       $pdo = new pdo("mysql:host=localhost; dbname=salaberga001","root","");
       $consulta = "select * from usuarios where email =:email and senha =:senha"; 
