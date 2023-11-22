b1596768c6c90b5586f9d2f7cfe6e414eb2115f1
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2023-11-22 14:07:50 -0300

    b

---
+++ 22-11-23/model/Usuario.class.php
@@ -0,0 +1,76 @@
+<?php
+
+  Class Usuario{
+    public function cadastrarUsuario ($email, $senha){
+         $pdo = new pdo("mysql:host=localhost; dbname=salaberga0 01","root","");
+         $consulta = "insert into usuarios values(null,:email,:senha)";
+
+        $consultar = $pdo->prepare($consulta);
+        $consultar->bindValue(":email", $email);
+        $consultar->bindValue(":senha", $senha);
+        $consultar->execute();
+         
+    }
+    public function recSenha ($email){
+      $pdo = new pdo("mysql:host=localhost; dbname=salaberga001","root","");
+      $consulta = "select * from usuarios where email =:email";
+     $consultar = $pdo->prepare($consulta);
+     $consultar->bindValue(":email", $email);
+
+     $consultar->execute();
+     foreach ($consultar as $value){
+        echo  "Senha = " .$value['senha'];
+      }
+    }
+    public function relatorio (){
+      $pdo = new pdo("mysql:host=localhost; dbname=salaberga001","root","");
+      $consulta = "select nome, valor from alunos inner join contato on alunos.id = contato.id_aluno";
+
+     $consultar = $pdo->prepare($consulta);
+     $consultar->execute();
+
+     echo '<table border=\'1\'>';
+     echo '<tr><td>Nome</td><td>Contato</td>';
+
+     foreach ($consultar as $value){
+        echo '<tr><td>' . $value['nome'] . '</td>'; 
+        echo '<td>' . $value['valor'] . '</td></tr>'; 
+     }
+     echo '</table>';
+    }
+
+    public function relatorioNotas (){
+      $pdo = new pdo("mysql:host=localhost; dbname=escola_201123","root","");
+      $consulta = "select aluno.nome, notas.nota, disciplina.nome_disc, professor.nome_prof from aluno inner join notas on aluno.id = notas.id_aluno inner join disciplina on notas.id_diciplina = disciplina.id inner join professor on disciplina.id_professor = professor.id";
+
+     $consultar = $pdo->prepare($consulta);
+     $consultar->execute();
+     
+     echo '<table border=\'1\'>';
+     echo '<tr><td>Nome</td><td>Nota</td><td>Disciplina</td><td>Proffesor</td>';
+    //  print_r($consultar);
+     foreach ($consultar as $value){
+       echo '<tr><td>' . $value[0] . '</td>'; 
+       echo '<td>'     . $value[1] . '</td>';
+       echo '<td>'     . $value[2] . '</td>';
+       echo '<td>'     . $value[3] . '</td></tr>';
+
+      }
+     echo '</table>';
+    }
+
+    public function autenticar($email, $senha){
+      $pdo = new pdo("mysql:host=localhost; dbname=salaberga001","root","");
+      $consulta = "select * from usuarios where email =:email and senha =:senha"; 
+     $consultar = $pdo->prepare($consulta);
+     $consultar->bindValue(":email", $email);
+     $consultar->bindValue(":senha", $senha);
+     $consultar->execute();
+     $result = $consultar->rowCount();
+     if($result>0){
+     header("location:https://www.google.com"); 
+    }else{
+        header("location:https://www.instagram.com");
+      }
+  }
+}
---
+++ 22-11-23/view/relatoriousuario.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Usuários</title>
+    <link rel="stylesheet" href="css/cadastro.css">
+</head>
+<body>
+    <main>
+        <div class="caixas">
+            <form action="../control/controleUsuario.php" method="POST"> 
+                <input type="submit" name="btn" value="Relatorio Usuario"> 
+                <input type="hidden" name="id" value="2"> 
+
+                <input type="submit" name="btn" value="Relatorio notas"> 
+                <input type="hidden" name="id" value="2.1"> 
+            </form>
+        </div>
+    </main>
+</body>
+</html>
