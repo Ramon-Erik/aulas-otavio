a5065cf6fdb92e57791efc767c9871b472574f62
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-02-20 10:45:51 -0300

    aula 20-02

---
+++ projeto 24.1/view/registro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGISTRO</title>
+</head>
+
+<body>
+    <center>
+        <form action="../control/controle-registro.php" method="POST">
+            <select name="ano">
+                <option value="1°"> 1° ANO </option>
+                <option value="2°"> 2° ANO </option>
+                <option value="3°"> 3° ANO </option>
+            </select>
+        </form>
+    </center>
+</body>
+
+</html>
