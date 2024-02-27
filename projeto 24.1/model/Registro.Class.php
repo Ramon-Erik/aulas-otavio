73af7642d1371cc6a934417302fa358e54bfd1a7
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-02-27 14:27:06 -0300

    Update Registro.Class.php

--- projeto 24.1/model/Registro.Class.php
+++ projeto 24.1/model/Registro.Class.php
@@ -1,17 +1,16 @@
 <?php
 class Registro {
     public function cadastrar_registro($motivo, $aluno) {
-     
-            $pdo = new pdo("mysql:host=localhost; dbname=registro_atraso_ramon", "root", "");
-            $consulta = "INSERT INTO registro VALUES (null,:motivo, curdate(),:id_aluno,:id_usuario)";
 
-            $id_usuario = 1;
+        $pdo = new pdo("mysql:host=localhost; dbname=registro_atraso_ramon", "root", "");
+        $consulta = "INSERT INTO registro VALUES (null,:motivo, curdate(),:id_aluno,:id_usuario)";
 
-            $consulta_feita = $pdo->prepare($consulta);
-            $consulta_feita->bindValue(":id_aluno", $aluno);
-            $consulta_feita->bindValue(":id_usuario", $id_usuario);
-            $consulta_feita->bindValue(":motivo", $motivo);
-            $consulta_feita->execute();
+        $id_usuario = 1;
+
+        $consulta_feita = $pdo->prepare($consulta);
+        $consulta_feita->bindValue(":id_aluno", $aluno);
+        $consulta_feita->bindValue(":id_usuario", $id_usuario);
+        $consulta_feita->bindValue(":motivo", $motivo);
+        $consulta_feita->execute();
+    }
 }
-}
-?>
