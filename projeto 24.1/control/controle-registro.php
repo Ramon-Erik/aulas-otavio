b7a331009eaa20b3d467163e414c8bf09160b509
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-02-28 13:43:56 -0300

    Update controle-registro.php

--- projeto 24.1/control/controle-registro.php
+++ projeto 24.1/control/controle-registro.php
@@ -1,12 +1,11 @@
 <?php
 require('../model/Registro.Class.php');
 
-if(isset($_POST['avanco'])){
-    $ano = $_POST['ano'];
-    $turma = $_POST['turma'];
-echo $ano;
-echo $turma;
-   // $registro = new Registro();
+if (isset($_POST['avanco'])) {
+  $ano = $_POST['ano'];
+  $turma = $_POST['turma'];
+  echo $ano;
+  echo $turma;
+  // $registro = new Registro();
   //  $registro->cadastrar_registro($motivo, $aluno);
 }
-?>
