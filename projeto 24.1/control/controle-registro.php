5ef1395204fc1187c944e9ddc735fa28123fbc58
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-02-28 14:11:31 -0300

    Update controle-registro.php

--- projeto 24.1/control/controle-registro.php
+++ projeto 24.1/control/controle-registro.php
@@ -1,6 +1,7 @@
 <?php
 require('../model/Registro.Class.php');
 
+
 if (isset($_POST['prox'])) {
   $ano = $_POST['ano'];
   $turma = $_POST['turma'];
@@ -10,7 +11,16 @@ if (isset($_POST['prox'])) {
   $_SESSION['turma'] = $turma;
 
   header("location: ../view/cadastrar-registro-2.php");
+}
 
-  // $registro = new Registro();
-  //  $registro->cadastrar_registro($motivo, $aluno);
+if (isset($_POST['cadastro_atraso'])) {
+  $id_aluno = $_POST['id_aluno'];
+  $motivo = $_POST['motivo'];
+
+  session_start();
+  $_SESSION['id_aluno'] = $id_aluno;
+  $_SESSION['motivo'] = $motivo;
+
+  $registro = new Registro();
+  $registro->cadastrar_registro($motivo, $id_aluno);
 }
