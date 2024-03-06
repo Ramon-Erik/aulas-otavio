0447ffa0420fb237fa8859a941ce4025341266e0
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-03-06 14:55:59 -0300

    mey senhor misericoria

--- projeto 24.1/view/relatorios/relatorio.php
+++ projeto 24.1/view/relatorios/relatorio.php
@@ -11,6 +11,13 @@ $consulta = "select * from aluno";
 $consulta_feita = $pdo->prepare($consulta);
 $consulta_feita->execute();
 
+$pdf->Image('imgs/html.png', 10, 0, 20, 0, 'PNG');
+$pdf->Cell(0,10,"                                  EEEP Salaberga Torquato Gomes de Matos", 0, 1, "L");
+$pdf->Cell(0, 10, "                                         EMI em informatica", 0, 1, "L");
+$pdf->Ln(10);
+$pdf->Cell(0, 5, utf8_decode ("Relatório de Alunos"), 0, 0, "C");
+$pdf->Ln(8);
+
 
 $pdf->Cell(20, 5, 'Id', 1, 0, 'L');
 $pdf->Cell(170, 5, 'Nome', 1, 1, 'L');
