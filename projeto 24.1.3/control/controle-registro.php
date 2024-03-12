3dc69094b3f53d10f0f328f159e8d87f9283dfd0
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-03-12 20:05:30 -0300

    formatação do doc

--- projeto 24.1.3/control/controle-registro.php
+++ projeto 24.1.3/control/controle-registro.php
@@ -1,38 +1,41 @@
 <?php
+
 require('../model/Registro.Class.php');
 
 if (isset($_POST['registro_turma'])) {
-  $ano = $_POST['ano'];
-  $turma = $_POST['turma'];
+    $ano = $_POST['ano'];
+    $turma = $_POST['turma'];
 
-  session_start();
-  $_SESSION['ano'] = $ano;
-  $_SESSION['turma'] = $turma;
+    session_start();
+    $_SESSION['ano'] = $ano;
+    $_SESSION['turma'] = $turma;
 
-  header("location:../view/relatorios/relatorio-registro.php");
+    header("location:../view/relatorios/relatorio-registro.php");
 }
 
 if (isset($_POST['prox'])) {
-  $ano = $_POST['ano'];
-  $turma = $_POST['turma'];
+    $ano = $_POST['ano'];
+    $turma = $_POST['turma'];
 
-  session_start();
-  $_SESSION['ano'] = $ano;
-  $_SESSION['turma'] = $turma;
-  // echo  'ano', $ano, 'turma', $turma;
-  // echo  'ano', $_SESSION['ano'], 'turma', $_SESSION['turma'];
+    session_start();
+    $_SESSION['ano'] = $ano;
+    $_SESSION['turma'] = $turma;
+    // echo  'ano', $ano, 'turma', $turma;
+    // echo  'ano', $_SESSION['ano'], 'turma', $_SESSION['turma'];
 
-  header("location: ../view/cadastrar-registro-2.php");
+    header("location: ../view/cadastrar-registro-2.php");
 }
 
 if (isset($_POST['cadastro_atraso'])) {
-  $id_aluno = $_POST['id_aluno'];
-  $motivo = $_POST['motivo'];
+    $id_aluno = $_POST['id_aluno'];
+    $motivo = $_POST['motivo'];
 
-  session_start();
-  $_SESSION['id_aluno'] = $id_aluno;
-  $_SESSION['motivo'] = $motivo;
+    session_start();
+    $_SESSION['id_aluno'] = $id_aluno;
+    $_SESSION['motivo'] = $motivo;
 
-  $registro = new Registro();
-  $registro->cadastrar_registro($motivo, $id_aluno);
+    $registro = new Registro();
+    $registro->cadastrar_registro($motivo, $id_aluno);
+
+    header("location: ../view/cadastrar-registro-2.php");
 }
