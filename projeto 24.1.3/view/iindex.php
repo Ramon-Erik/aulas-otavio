646f3a70038635a8e13cc7029a64002a00703443
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-03-12 21:31:13 -0300

    index fixed display; error links

--- projeto 24.1.3/view/iindex.php
+++ projeto 24.1.3/view/iindex.php
@@ -9,8 +9,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
     <link rel="shortcut icon" href="assets/images/logo4.png" type="image/x-icon">
     <meta name="description" content="Web Builder Description">
-
-
     <title>Page 5</title>
     <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
@@ -23,13 +21,10 @@
     <link rel="stylesheet" href="assets/theme/css/style.css">
     <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
     <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
-
-
-
 </head>
 
 <body>
-    <section class="menu cid-u6mG0V4yGt" once="menu" id="menu1-19">
+<section class="menu cid-u6mKbH6svl" once="menu" id="menu1-1e">
         <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <div class="hamburger">
@@ -52,25 +47,12 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true">
                     <li class="nav-item dropdown">
-                        <a class="nav-link link text-white dropdown-toggle display-4" href="cadastrar-usuario.php" data-toggle="dropdown-submenu">
-                            <span class="mbri-upload mbr-iconfont mbr-iconfont-btn"></span>
-                            Cadastrar-aquii
-                        </a>
-                        <div class="dropdown-menu">
-                            <a class="text-white dropdown-item display-4" href="cadastrar-usuario.php">Usuarios</a>
-                            <a class="text-white dropdown-item display-4" href="cadastrar-aluno.php">Alunos</a>
-                            <a class="text-white dropdown-item display-4" href="cadastrar-registro.php">Registros</a>
-                        </div>
+                        <a class="nav-link link text-white dropdown-toggle display-4" href="https://mobirise.co" data-toggle="dropdown-submenu"><span class="mbri-upload mbr-iconfont mbr-iconfont-btn"></span>
+                            Cadastrar</a>
+                        <div class="dropdown-menu"><a class="text-white dropdown-item display-4" href="https://mobirise.co">Usuarios<br>Alunos<br>Registros</a></div>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link link text-white dropdown-toggle display-4" href="relatorio-usuario.php" data-toggle="dropdown-submenu">
-                            <span class="mbri-edit mbr-iconfont mbr-iconfont-btn"></span>
-                            Relatorio
-                        </a>
-                        <div class="dropdown-menu">
-                            <a class="text-white dropdown-item display-4" href="https://mobirise.co">Usuarios<br>Alunos<br>Registro</a></div>
-                            <a class="text-white dropdown-item display-4" href="https://mobirise.co">Usuarios<br>Alunos<br>Registro</a></div>
-                            <a class="text-white dropdown-item display-4" href="https://mobirise.co">Usuarios<br>Alunos<br>Registro</a></div>
+                    <li class="nav-item dropdown"><a class="nav-link link text-white dropdown-toggle display-4" href="https://mobirise.co" data-toggle="dropdown-submenu"><span class="mbri-edit mbr-iconfont mbr-iconfont-btn"></span>Relatorio</a>
+                        <div class="dropdown-menu"><a class="text-white dropdown-item display-4" href="https://mobirise.co">Usuarios<br>Alunos<br>Registro</a></div>
                     </li>
                     <li class="nav-item"><a class="nav-link link text-white display-4" href="https://mobirise.co">
                         </a></li>
