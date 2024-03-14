b879402269993bf6b93e093ca81bd2728cbcb4ac
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-03-13 23:04:10 -0300

    Create login.php

---
+++ projeto 24.1/view/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Entre em uma conta sua!</title>
+    <link rel="stylesheet" href="css/registro.css">
+</head>
+<body>
+    <main>
+        <h1>Faça login em sua conta!</h1>
+        <form action="../control/controle-login.php" method="post">
+            <div class="linha">
+                <label for="emailId">Informe seu email:</label>
+            </div>
+            <div class="linha">
+                <input type="email" name="email" id="emailId" placeholder="exemplo@gmail.com" required>
+            </div>
+            <div class="linha">
+                <label for="senhaId">Informe sua senha:</label>
+            </div>
+            <div class="linha">
+                <input type="password" name="senha" id="senhaId" required>
+            </div>
+            <div class="linha">
+                <input type="submit" name="logar" id="logarId" value="Fazer login" required>
+            </div>
+        </form>
+    </main>
+</body>
+</html>
