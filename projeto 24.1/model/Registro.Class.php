16ae1fb445eaef79608fb3e7b94d343390574ea8
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-03-13 23:05:04 -0300

    Update Registro.Class.php

--- projeto 24.1/model/Registro.Class.php
+++ projeto 24.1/model/Registro.Class.php
@@ -1,29 +1,25 @@
 <?php
-class Registro
-{
-    public function cadastrar_registro($motivo, $aluno)
-    {
-
+class Registro {
+    public function cadastrar_registro($motivo, $aluno) {
         $pdo = new pdo("mysql:host=localhost; dbname=registro_atraso_ramon", "root", "");
         $consulta = "INSERT INTO registro VALUES (null,:motivo, curdate(),:id_aluno,:id_usuario)";
 
-        $id_usuario = 1;
+        session_start();
 
         $consulta_feita = $pdo->prepare($consulta);
         $consulta_feita->bindValue(":id_aluno", $aluno);
-        $consulta_feita->bindValue(":id_usuario", $id_usuario);
+        $consulta_feita->bindValue(":id_usuario", $_SESSION['id_usuario']);
         $consulta_feita->bindValue(":motivo", $motivo);
         $consulta_feita->execute();
 
         header("location: ../view/cadastrar-registro-1.php");
     }
 
-    public function relatorio_turma()
-    {
+    public function relatorio_turma() {
         require('../../../LIB_FPDF/fpdf.php');
 
         session_start();
-   
+
         $pdf = new FPDF();
 
         $pdf->AddPage();
@@ -46,7 +42,7 @@ class Registro
         foreach ($consulta_feita as $reg) {
             $pdf->Cell(80, 5, utf8_decode($reg['nome']), 1, 0, 'L');
             $pdf->Cell(25, 5, utf8_decode($reg['ano']), 1, 0, 'L');
-            $pdf->Cell(25, 5, utf8_decode ($reg['turma']), 1, 0, 'C');
+            $pdf->Cell(25, 5, utf8_decode($reg['turma']), 1, 0, 'C');
             $pdf->Cell(25, 5, utf8_decode($reg['data']), 1, 0, 'C');
             $pdf->Cell(25, 5, utf8_decode($reg['motivo']), 1, 1, 'C');
             $pdf->SetFont('arial', '', 12);
