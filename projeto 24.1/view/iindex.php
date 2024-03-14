3e43672ffe26558c327fe390be0ac48b9c54ff02
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-03-13 23:04:27 -0300

    Create iindex.php

---
+++ projeto 24.1/view/iindex.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página inicial - Atrasos</title>
+    <link rel="stylesheet" href="css/registro.css">
+</head>
+<body>
+    <main>
+        <h1>O que você quer fazer?</h1>
+        <div class="links">
+            <ul>
+                <li>
+                    <a href="login.php">Login</a>
+                </li>
+                <li>
+                    <a href="#">Cadastrar usuário</a>
+                </li>
+                <li>
+                    <a href="#">Cadastrar aluno</a>
+                </li>
+                <li>
+                    <a href="#">Regstrar atraso</a>
+                </li>
+                <li>
+                    <a href="#">Gerar relatório</a>
+                </li>
+            </ul>
+        </div>
+    </main>
+</body>
+</html>
